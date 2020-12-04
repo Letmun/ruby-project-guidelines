778805ffb15de7b03be6289b1cffa26bcbbcf831
Brian Li <Brian.Li96@yahoo.com>
2020-12-04 09:48:09 -0600

    not done

--- bin/run.rb
+++ bin/run.rb
@@ -1,33 +1,77 @@
 require_relative '../config/environment'
 require "tty-prompt"
 
-puts "HELLO WORLD"
+class AppCLI
 
-prompt = TTY::Prompt.new
-puts "Welcome to the D&D Character Creation List!"
-user_name = prompt.ask("What would you like to be called?")
-user_name = Player.new(name = user_name)
+    def run 
+        welcome
+    end
 
-puts "Hello #{user_name}!"                                      #Inputting player name & creating player instance
+    def welcome
+        prompt = TTY::Prompt.new
+        puts "Welcome to the D&D Character Creation List!"
+        puts "What would you like to be called?"
+        user_name = gets.chomp
+        @player_name = Player.create(name: user_name) 
+        puts "Hello #{@player_name.name}!"
+        create_character
+    end
 
-character_classes = Feature.all.map {|feature| feature.class_name}
+    def create_character
+        prompt = TTY::Prompt.new
+        puts "What is your character's name?"
+        character_name = gets.chomp
+        puts "Please select your race: 1) Dragonborn   2) Elf   3) Dwarf   4) Gnome   5) Half-Elf   6) Halfling   7) Half-Orc   8) Human   9) Tielfing"
+        character_race = gets.chomp
+        # all_races = CharacterSheet.all.map {|race| @character_sheet.race}
+        # character_race = prompt.enum_select("Please select your race:", all_races)
+        @character_sheet = CharacterSheet.create(name: character_name, race: character_race, player_id: @player_name.id)        #Created character sheet instance
+        
+        puts "Hello #{@character_sheet.name}!"
+        create_feature
 
-character_name = prompt.ask("Please enter your character's name: ")   #Create character & creating character_sheet instance
-character_race = prompt.ask("Please enter your character's race: ")
-selected_character_class = prompt.select("Please select your character class", character_classes)
+    end
 
-if character_class == "Fighter"
+    def create_feature
+        puts "Please select your class: 1) Fighter   2) Rogue   3) Mage    4) Hunter"
+        character_class = gets.chomp
+        puts "Please select your ability: "
+        character_ability = gets.chomp
 
-elsif character_class == "Mage"
+        @feature = Feature.new(class_name: character_class, ability: character_ability)
+        @joiner = CharacterSheetFeatureJoiner.new(character_sheets_id: @character_sheet.id, features_id: @feature.id)
+    end
 
-elsif character_class == "Hunter"
 
-elsif character_class == "Rogue"
+
+
+
+
+                                     #Inputting player name & creating player instance
+
+# character_classes = Feature.all.map {|feature| feature.class_name}
+
+# character_name = prompt.ask("Please enter your character's name: ")   #Create character & creating character_sheet instance
+# character_race = prompt.ask("Please enter your character's race: ")
+# selected_character_class = prompt.select("Please select your character class", character_classes)
+
+# if character_class == "Fighter"
+
+# elsif character_class == "Mage"
+
+# elsif character_class == "Hunter"
+
+# elsif character_class == "Rogue"
 
 end
 
-new_character = Charactersheet.new(name = character_name, race = character_race, features_id = selected_character_class, player_id = user_name.id)
+# new_character = Charactersheet.new(name = character_name, race = character_race, features_id = selected_character_class, player_id = user_name.id)
 
+
+
+
+
+AppCLI.new.run
 
 
 
--- lib/character_sheet.rb
+++ lib/character_sheet.rb
@@ -1,5 +1,11 @@
 class CharacterSheet < ActiveRecord::Base
     belongs_to :player  
     has_many :feature, through: :character_sheet_feature_joiner
+
+    def race
+        race = ["Dragonborn", "Elf", "Dwarf", "Gnome", "Half-Elf", "Halfling", "Half-Orc", "Human", "Tielfing"] 
+        all_races = self.all.map {|race| @character_sheet.race}
+        character_race = prompt.enum_select("Please select your race:", all_races)
+    end
  end
  
